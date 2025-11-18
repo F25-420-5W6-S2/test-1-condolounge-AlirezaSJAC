21db2549b503934212b32b8d77804ab1f45da1f3
Satvati, Alireza <2389790@johnabbottcollege.net>
2025-11-18 13:11:34 -0500

    Added specific repositories

---
+++ CondoLounge/Data/Repositories/BuildingRepository.cs
@@ -0,0 +1,11 @@
+﻿using CondoLounge.Data.Entities;
+
+namespace CondoLounge.Data.Repositories
+{
+    public class BuildingRepository : CondoLoungeGenericGenericRepository<Building>
+    {
+        public BuildingRepository(ApplicationDbContext db, ILogger<CondoLoungeGenericGenericRepository<Building>> logger) : base(db, logger)
+        {
+        }
+    }
+}
